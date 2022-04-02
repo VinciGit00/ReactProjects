559eb0b6f029ee4247f09cd4b2ca9e7a1ba62429
Marco Vinciguerra <88108002+VinciGit00@users.noreply.github.com>
2022-04-02 18:53:29 +0200

    aggiunta possibilità di mandare mail

--- React/react-portfolio/src/components/Contact/index.js
+++ React/react-portfolio/src/components/Contact/index.js
@@ -1,41 +1,44 @@
-import { useEffect, useState } from 'react'
-import Loader from 'react-loaders'
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
-import { useRef } from 'react'
-import emailjs from '@emailjs/browser'
-import AnimatedLetters from '../AnimatedLetters'
-import './index.scss'
+import { useEffect, useState } from "react";
+import Loader from "react-loaders";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { useRef } from "react";
+import emailjs from "@emailjs/browser";
+import AnimatedLetters from "../AnimatedLetters";
+import "./index.scss";
 
 const Contact = () => {
-  const [letterClass, setLetterClass] = useState('text-animate')
-  const form = useRef()
+  const [letterClass, setLetterClass] = useState("text-animate");
+  const form = useRef();
 
   useEffect(() => {
     return setTimeout(() => {
-      setLetterClass('text-animate-hover')
-    }, 3000)
-  }, [])
+      setLetterClass("text-animate-hover");
+    }, 3000);
+  }, []);
 
   const sendEmail = (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
     emailjs
       .sendForm(
-        'gmail',
-        'template_YeJhZkgb',
+        "service_mrk0h4p",
+        "template_x5bmvhc",
         form.current,
-        'your-token'
+        "nW6LUTi3pEck0RPd-"
       )
+      //emailjs
+      // .sendForm("service_ls6d3ox", "template_x5bmvhc")
+
       .then(
         () => {
-          alert('Message successfully sent!')
-          window.location.reload(false)
+          alert("Message successfully sent!");
+          window.location.reload(false);
         },
         () => {
-          alert('Failed to send the message, please try again')
+          alert("Failed to send the message, please try again");
         }
-      )
-  }
+      );
+  };
 
   return (
     <>
@@ -44,14 +47,15 @@ const Contact = () => {
           <h1>
             <AnimatedLetters
               letterClass={letterClass}
-              strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']}
+              strArray={["C", "o", "n", "t", "a", "c", "t", " ", "m", "e"]}
               idx={15}
             />
           </h1>
           <p>
             I am interested in freelance opportunities - especially ambitious or
             large projects. However, if you have other request or question,
-            don't hesitate to contact me using below form either.
+            don't hesitate to contact me using below form either. If the service
+            does not work, please contact me at mvincig11@gmail.com
           </p>
           <div className="contact-form">
             <form ref={form} onSubmit={sendEmail}>
@@ -89,28 +93,10 @@ const Contact = () => {
             </form>
           </div>
         </div>
-        <div className="info-map">
-          Slobodan Gajić,
-          <br />
-          Serbia,
-          <br />
-          Branka RadiČevića 19, 22000 <br />
-          Sremska Mitrovica <br />
-          <br />
-          <span>freelancerslobodan@gmail.com</span>
-        </div>
-        <div className="map-wrap">
-          <MapContainer center={[44.96366, 19.61045]} zoom={13}>
-            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-            <Marker position={[44.96366, 19.61045]}>
-              <Popup>Sloba lives here, come over for a cup of coffee :)</Popup>
-            </Marker>
-          </MapContainer>
-        </div>
       </div>
       <Loader type="pacman" />
     </>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
