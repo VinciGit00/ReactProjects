55736755c2341796d7557189074b9d45f639057f
Marco Vinciguerra <88108002+VinciGit00@users.noreply.github.com>
2022-04-03 21:07:19 +0200

    added animations

---
+++ React/timeline/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import React from "react";
+import Timeline from "./components/timeline";
+
+const App = () => (
+  <>
+    <h1>React Timeline</h1>
+    <Timeline />
+  </>
+);
+
+export default App;
