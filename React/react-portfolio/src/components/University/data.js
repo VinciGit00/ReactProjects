e58697b9fd6a603343869ec1c7b88f964636896e
Marco Vinciguerra <88108002+VinciGit00@users.noreply.github.com>
2022-04-04 13:45:40 +0200

    added timeline

---
+++ React/react-portfolio/src/components/University/data.js
@@ -0,0 +1,48 @@
+const timelineData = [
+  {
+    text: "Started working on the app-ideas repository",
+    date: "February 25 2019",
+    category: {
+      tag: "app-ideas",
+      color: "#FFDB14",
+    },
+    link: {
+      url: "https://github.com/VinciGit00",
+      text: "Check it out on GitHub",
+    },
+  },
+  {
+    text: "Started the Weekly Coding Challenge program",
+    date: "March 04 2019",
+    category: {
+      tag: "blog",
+      color: "#e17b77",
+    },
+  },
+  {
+    text: "Prova",
+    date: "March 07 2019",
+    category: {
+      tag: "twitter",
+      color: "#1DA1F2",
+    },
+    link: {
+      url: "https://twitter.com/florinpop1705",
+      text: "See profile",
+    },
+  },
+  {
+    text: "Testo",
+    date: "March 18 2019",
+    category: {
+      tag: "medium",
+      color: "#018f69",
+    },
+    link: {
+      url: "https://medium.freecodecamp.org/how-to-build-a-double-slider-sign-in-and-sign-up-form-6a5d03612a34",
+      text: "Check it out here",
+    },
+  },
+];
+
+export default timelineData;
--- React/react-portfolio/src/components/University/index.js
+++ React/react-portfolio/src/components/University/index.js
@@ -1,79 +1,19 @@
-import { useEffect, useState } from "react";
-import {
-  faAngular,
-  faCss3,
-  faGitAlt,
-  faHtml5,
-  faJsSquare,
-  faReact,
-} from "@fortawesome/free-brands-svg-icons";
-import Loader from "react-loaders";
-import AnimatedLetters from "../AnimatedLetters";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import timelineData from "./data";
+import TimelineItem from "./timelineItem";
 import "./index.scss";
 
-const University = () => {
-  const [letterClass, setLetterClass] = useState("text-animate");
-
-  useEffect(() => {
-    return setTimeout(() => {
-      setLetterClass("text-animate-hover");
-    }, 3000);
-  }, []);
-
-  return (
-    <>
-      <div className="container about-page">
+const University = () =>
+  timelineData.length > 0 && (
+    <div className="container contact-page">
+      <div className="timeline-container">
         <div className="text-zone">
-          <h1>
-            <AnimatedLetters
-              letterClass={letterClass}
-              strArray={["A", "b", "o", "u", "t", " ", "m", "e"]}
-              idx={15}
-            />
-          </h1>
-          <p>
-            I'm very ambitious front-end developer looking for a role in
-            established IT company with the opportunity to work with the latest
-            technologies on challenging and diverse projects.
-          </p>
-          <p align="LEFT">
-            I'm quietly confident, naturally curious, and perpetually working on
-            improving my chops one design problem at a time.
-          </p>
-          <p>
-            If I need to define myself in one sentence that would be a family
-            person, father of a beautiful daughter, a sports fanatic,
-            photography enthusiast, and tech-obsessed!!!
-          </p>
-        </div>
-
-        <div className="stage-cube-cont">
-          <div className="cubespinner">
-            <div className="face1">
-              <FontAwesomeIcon icon={faAngular} color="#DD0031" />
-            </div>
-            <div className="face2">
-              <FontAwesomeIcon icon={faHtml5} color="#F06529" />
-            </div>
-            <div className="face3">
-              <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
-            </div>
-            <div className="face4">
-              <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
-            </div>
-            <div className="face5">
-              <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
-            </div>
-            <div className="face6">
-              <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
-            </div>
-          </div>
+          {timelineData.map((data, idx) => (
+            <TimelineItem data={data} key={idx} />
+          ))}
         </div>
       </div>
-      <Loader type="pacman" />
-    </>
+    </div>
   );
-};
 
 export default University;
