e34f383591942f850ea62829696822043754f3fd
Marco Vinciguerra <88108002+VinciGit00@users.noreply.github.com>
2022-04-16 08:11:49 +0200

    added map

--- React/mongodb/client/src/App.js
+++ React/mongodb/client/src/App.js
@@ -6,6 +6,14 @@ function App() {
   const [food, setFoodname] = useState("");
   const [days, setDays] = useState(0);
 
+  useEffect(() => {
+    Axios.get("http://localhost:3001/read ").then((response) => {
+      setFoodList(response.data);
+    });
+  }, []);
+
+  const [foodList, setFoodList] = useState([]);
+
   const addToList = () => {
     console.log(food + days);
     Axios.post("http://localhost:3001/insert", { foodName: food, days: days });
@@ -31,6 +39,15 @@ function App() {
       <button className="button" onClick={addToList}>
         Add to list
       </button>
+      <h1>Food list</h1>
+      {foodList.map((val, key) => {
+        return (
+          <div>
+            <h2>{val.foodName}</h2>
+            <h2>{val.daySinceIAte}</h2>
+          </div>
+        );
+      })}
     </div>
   );
 }
