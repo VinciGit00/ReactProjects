6193682d55c4cd0c3bb2392aef9af60eec188710
Marco Vinciguerra <88108002+VinciGit00@users.noreply.github.com>
2022-04-22 12:04:39 +0200

    aggiunto filtro ricerche

--- React/search_filter/src/App.js
+++ React/search_filter/src/App.js
@@ -1,11 +1,28 @@
 import "./App.css";
 import JSONDATA from "./MOCK_DATA.json";
+import { useState } from "react";
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+
   return (
     <div className="App">
-      <input type="text" placeholder="Search..."></input>
-      {JSONDATA.map((val, key) => {
+      <input
+        type="text"
+        placeholder="Search..."
+        onChange={(event) => {
+          setSearchTerm(event.target.value);
+        }}
+      ></input>
+      {JSONDATA.filter((val) => {
+        if (searchTerm == "") {
+          return val;
+        } else if (
+          val.first_name.toLowerCase().includes(searchTerm.toLowerCase())
+        ) {
+          return val;
+        }
+      }).map((val, key) => {
         return (
           <div className="user" key={key}>
             <p>{val.first_name}</p>
