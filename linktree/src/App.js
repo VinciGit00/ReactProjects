0fd606ae6d7f0e449700cde278c68a6658e2e172
Marco Vinciguerra <88108002+VinciGit00@users.noreply.github.com>
2022-08-27 14:45:05 +0200

    finished linktree
    manca solo il colore dei design

--- linktree/src/App.js
+++ linktree/src/App.js
@@ -2,6 +2,7 @@ import "primereact/resources/themes/saga-blue/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeflex/primeflex.css";
 import "primeicons/primeicons.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 import { Avatar } from "primereact/avatar";
 import { Button } from "primereact/button";
@@ -16,7 +17,7 @@ function App() {
   const data = {
     name: "Marco Vinciguerra",
     image: "https://avatars.githubusercontent.com/u/88108002?v=4",
-    bio: "Master IT engeneer student",
+    bio: "Master IT engineer student",
     links: [
       {
         name: "My Github",
@@ -28,20 +29,21 @@ function App() {
         url: "https://www.linkedin.com/in/marco-vinciguerra-7ba365242/",
         icon: "linkedin",
       },
-      {
-        name: "My Notion sideprojects documentation",
-        url: "https://www.linkedin.com/in/marco-vinciguerra-7ba365242/",
-        icon: "notion",
-      },
+
       {
         name: "My portfolio website",
         url: "https://www.linkedin.com/in/marco-vinciguerra-7ba365242/",
         icon: "myWebsite",
       },
+      {
+        name: "My Notion sideprojects documentation",
+        url: "https://mvinciguerra.notion.site/Side-projects-155b47302bf74029b9fe44fb213647de",
+        icon: "notion",
+      },
     ],
   };
   return (
-    <div className="p-m-4">
+    <div className="p-m-4" style={{ paddingTop: 15 }}>
       <div
         className="p-d-flex p-jc-center p-ai-center"
         style={{
@@ -52,21 +54,38 @@ function App() {
         <Avatar image={data.image} size="xlarge" shape="circle"></Avatar>
         <h1 className="p-m-2">{data.name}</h1>
       </div>
-      <div className="p-d-flex p-jc-center p-ai-center">
+
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        {" "}
         <p>{data.bio}</p>
       </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+        }}
+      ></div>
       <div className="p-d-flex p-jc-center p-ai-center">
         <div className="p-d-flex p-flex-column">
           {data.links.map((link) => (
-            <a href={link.url} target="_blank" rel="noreferrer">
-              <Button
-                className="p-m-2 p-button-outlined"
-                style={{ color: colors[link.icon] }}
-              >
-                <i className={"pi pi-${link.icon} p-px-3"}></i>
-                {link.name}
-              </Button>
-            </a>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                paddingTop: 10,
+              }}
+            >
+              <a href={link.url} target="_blank" rel="noreferrer">
+                <button type="button" class="btn btn-warning">
+                  {link.name}
+                </button>
+              </a>
+            </div>
           ))}
         </div>
       </div>
